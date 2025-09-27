27dbc9fda9fe7db546463c6a722e33313762e799
Sri Nanak <sri@w.com>
2025-09-27 13:02:22 -0700

    added the dropdown and labels to prepare for the first connection to mysql for practical purposes

--- trial1.py
+++ trial1.py
@@ -14,21 +14,34 @@ def exit_trial():
 def clear_label():
     label.config(text=" ")
     entry_widget.delete(0, tki.END)
+    selected_option.set(options [0])
 
 # Create the main application window
 root = tki.Tk()
 root.title("Simple GUI Example")
+root.configure(bg="#FFBF00")
 root.geometry("400x250") # Set window size
 
 #name_variable= tk.StringVar()
 
 #data entry
-label = tki.Label(root, text= "Enter Name" )
+label = tki.Label(root, text= "Enter Name", fg="black")
 label.pack(pady=20) # Add padding for better spacing
 
 entry_widget = tki.Entry(root, width=10,bg="white", fg="green")
 entry_widget.pack(pady=5)
 
+#data entry 2
+label = tki.Label(root, text= "Enter gender", fg="black")
+label.pack(pady=20) # Add padding for better spacing
+
+options = ["Male", "Female", "Nonbinary"]
+
+selected_option = tki.StringVar(root)
+selected_option.set(options[0])  # Set default value
+
+dropdown = tki.OptionMenu(root, selected_option, *options)
+dropdown.pack(pady=10)
 
 
 # Create a label widget
@@ -50,8 +63,6 @@ button1 = tki.Button(root, text="Exit", command=exit_trial, fg="blue", bg="white
 #if on_button_click() = true
     #print("welcome")
 button1.pack()
-
-
 
 # Start the Tkinter event loop
 root.mainloop()
