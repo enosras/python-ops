58898db0498f63b62a621eff9e2dc466e75099c4
enos-python-dev <enya@w.com>
2025-09-27 12:39:16 -0700

    i added a delete clear and removed bugs from the exit commad

--- trial1.py
+++ trial1.py
@@ -1,4 +1,4 @@
-import tkinter as tk
+import tkinter as tki
 from time import sleep
 
 
@@ -7,37 +7,46 @@ def on_button_click():
     label.config(text="Button was clicked!")
 def exit_trial():
     #print(f'welcome and goodbye {namae} ')
-    label.config(text="welcome", textvariable=namae)
-    sleep(5)
+    name_variable=entry_widget.get()
+    label.config(text=f' Bye {name_variable} , your money is coming')
+    #sleep(3)
     exit()
+def clear_label():
+    label.config(text=" ")
+    entry_widget.delete(0, tki.END)
 
 # Create the main application window
-root = tk.Tk()
+root = tki.Tk()
 root.title("Simple GUI Example")
-root.geometry("400x200") # Set window size
+root.geometry("400x250") # Set window size
 
-namae= tk.StringVar()
+#name_variable= tk.StringVar()
 
 #data entry
-label = tk.Label(root, text= "Enter Name" )
+label = tki.Label(root, text= "Enter Name" )
 label.pack(pady=20) # Add padding for better spacing
 
-entry_widget = tk.Entry(root, width=10, textvariable=namae,bg="white", fg="green")
+entry_widget = tki.Entry(root, width=10,bg="white", fg="green")
 entry_widget.pack(pady=5)
-name=entry_widget.get()
+
 
 
 # Create a label widget
-label = tk.Label(root, text= "Karibu" )
+label = tki.Label(root, text= "Karibu" )
 label.pack(pady=20) # Add padding for better spacing
 
 # Create a button widget
-button = tk.Button(root, text="Click Me", command=on_button_click)
+button = tki.Button(root, text="Click Me", command=on_button_click)
 #if on_button_click() = true
     #print("welcome")
 button.pack()
 
-button1 = tk.Button(root, text="Exit", command=exit_trial, fg="blue", bg="white")
+button2 = tki.Button(root, text="Clear", command=clear_label)
+#if on_button_click() = true
+    #print("welcome")
+button2.pack()
+
+button1 = tki.Button(root, text="Exit", command=exit_trial, fg="blue", bg="white")
 #if on_button_click() = true
     #print("welcome")
 button1.pack()
